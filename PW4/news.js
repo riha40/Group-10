cb431e436d50d36fc93e35985dad287a5e44f4a9
riha40 <66122646+riha40@users.noreply.github.com>
2022-01-14 12:42:12 +0200

    Update news.js

--- PW4/news.js
+++ PW4/news.js
@@ -1,5 +1,5 @@
 const url =
-  "https://newsapi.org/v2/top-headlines?country=us&apiKey=4a90e53c4797486787e87a8d09819412";
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
 
 export async function getNews() {
   let result = await fetch(url).then(response => response.json());
